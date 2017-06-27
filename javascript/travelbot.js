033570f16193f6eb14067578ca0c2f5cee49ac2b
Hernan Tejada <hernan.tejada@gmail.com>
2017-06-27 19:50:41 -0400

    API responds

--- javascript/travelbot.js
+++ javascript/travelbot.js
@@ -1,18 +1,18 @@
 
 jQuery(document).ready(function() {
   $.ajax({
-  	url : "http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json",
+  	url : "http://api.wunderground.com/api/eb190ccc88c7b1f8/geolookup/conditions/q/IA/Cedar_Rapids.json",
   	dataType : "jsonp",
   }).done(function(response) {
   	console.log(response);
   });
 });
 	
-	
+/*	
 var weatherKey = "eb190ccc88c7b1f8"
 var seached = ""	
 var queryURL = "http://api.wunderground.com/api/" + weatherKey + "/geolookup/conditions/q/IA/" + serched + ".json" ;
-	
+	*/
 	
 	
 	
