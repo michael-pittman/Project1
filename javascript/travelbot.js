58f0e78b632bd063ae8607d8fbfdc93cdbcc54a6
michaelc.pittman <michaelc.pittman@gmail.com>
2017-06-27 19:44:28 -0400

    vinny's help

--- javascript/travelbot.js
+++ javascript/travelbot.js
@@ -1,19 +1,18 @@
 
-jQuery(document).ready(function($) {
+jQuery(document).ready(function() {
   $.ajax({
-  url : "http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json",
-  dataType : "jsonp",
-  success : function(parsed_json) {
-  var location = parsed_json['location']['city'];
-  var temp_f = parsed_json['current_observation']['temp_f'];
-  alert("Current temperature in " + location + " is: " + temp_f);
-  }
+  	url : "http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json",
+  	dataType : "jsonp",
+  }).done(function(response) {
+  	console.log(response);
   });
 });
 	
 	
-var queryURL = "http://api.wunderground.com/api/" + weatherKey + "/geolookup/conditions/q/IA/" + serched + ".json" ;
 var weatherKey = "eb190ccc88c7b1f8"
 var seached = ""	
-	console.log("called")
+var queryURL = "http://api.wunderground.com/api/" + weatherKey + "/geolookup/conditions/q/IA/" + serched + ".json" ;
+	
+	
+	
 	
