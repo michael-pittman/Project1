0e7b026a4e4e66a0db3ba1b9cb88d6d0fef96970
michaelc.pittman <michaelc.pittman@gmail.com>
2017-06-27 20:20:13 -0400

    results var added

--- javascript/travelbot.js
+++ javascript/travelbot.js
@@ -1,18 +1,29 @@
 
 $(document).ready(function() {
-  $.ajax({
-  	url : "http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/IA/Cedar_Rapids.json",
-  	dataType : "jsonp",
+//var seached = "Washington"	
+var queryURL = "http://api.wunderground.com/api/eb190ccc88c7b1f8/geolookup/conditions/q/IA/Washington.json" ;
+	
+	
+	
+	$.ajax({
+  	url : queryURL
+//  	dataType : "jsonp",
   }).done(function(response) {
-  	console.log(response);
+  	
+	var location = response.location.city;
+	var temp_f = response.current_observation.temp_f;
+	var temp_c = response.current_observation.temp_c;
+	console.log(response);
+	console.log(location);
+	console.log(temp_f);
+	console.log(temp_c);
   });
 });
 	
 	
-var weatherKey = "eb190ccc88c7b1f8"
-var seached = ""	
-var queryURL = "http://api.wunderground.com/api/" + weatherKey + "/geolookup/conditions/q/IA/" + serched + ".json" ;
-	
+//var weatherKey = "eb190ccc88c7b1f8"
+
+//	
 	
 	
 	
