e79945f36fba2871dd4b60b0fe18aba21713424b
Hernan Tejada <hernan.tejada@gmail.com>
2017-06-29 21:01:36 -0400

    First API connected to html request and result

--- javascript/travelbot.js
+++ javascript/travelbot.js
@@ -1,21 +1,30 @@
-$(document).ready(function() {
-var seached = "lawton"
+
+$("#add-entry").on("click", function(event) {
+    event.preventDefault();
+    var seached = $("#city-input").val().trim();
+      
 	//$(#ofinputfeild).html(val().trim)
-var queryURL = "http://api.wunderground.com/api/eb190ccc88c7b1f8/geolookup/conditions/q/IA/" + seached + ".json";
+	var queryURL = "http://api.wunderground.com/api/eb190ccc88c7b1f8/geolookup/conditions/q/IA/" + seached + ".json";
 	
 	$.ajax({
-  	url : queryURL
+	  	url : queryURL,
+	  	method: "GET"
 	
-  }).done(function(response) {
-  	
-	var location = response.location.city;
-	var temp_f = response.current_observation.temp_f;
-	var temp_c = response.current_observation.temp_c;
-	console.log(response);
-	console.log(location);
-	console.log(temp_f);
-	console.log(temp_c);
-  });
+	}).done(function(response) {
+		var city = response.location.city;
+		var country = response.location.country;
+		var temp_f = response.current_observation.temp_f;
+		var temp_c = response.current_observation.temp_c;
+		$("#fahrenheit").append(temp_f + "F°");
+		$("#celsius").append(temp_c + "C°");
+		$("#cityResult").append(city);
+		$("#countryResult").append(country);
+		console.log(response);
+		console.log(city);
+		console.log(country);
+		console.log(temp_f);
+		console.log(temp_c);
+	});
 });
 	
 	
