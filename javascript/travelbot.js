89d75ffbb91ce34e4a7be2352751c014b47597ef
Hernan Tejada <hernan.tejada@gmail.com>
2017-07-03 01:59:45 -0400

    autocomplete lines of code

--- javascript/travelbot.js
+++ javascript/travelbot.js
@@ -1,15 +1,64 @@
 
+function resetEntries() {
+	$("#city-input").val("");
+	$("#country-input").val("");
+};
+
+function resetResults() {
+	$("#fahrenheit").html("");
+	$("#celsius").html("");
+	$("#cityResult").html("");
+	$("#countryResult").html("");
+};
+
+$(document).ready(function(){
+	var firstLetters;
+	var cityElements;
+	var countryElements;
+	var cityList = [];
+	var countryList = [];
+	var selectedIndex;
+
+	$("#city-input").keyup(function() {
+		firstLetters = $(this).val().trim();
+		var autocompleteURL = "http://autocomplete.wunderground.com/aq?&cb=call=?";
+
+		$.ajax({
+			url: autocompleteURL,
+			dataType: "jsonp",
+			data: {
+				"query": firstLetters
+			},
+			crossDomain: true
+
+		}).done(function(response) {			
+			$.each(response.RESULTS, function(i, element){
+				cityElements = (response.RESULTS[i].name);
+				countryElements = (response.RESULTS[i].c);
+				cityList.push(cityElements);
+				countryList.push(countryElements);
+			});
+			$("#city-input").autocomplete({
+				source: cityList,
+				select: function(event, ui){
+					selectedIndex = $.inArray(ui.item.value,$("#city-input").autocomplete("option", "source"));
+					$("#country-input").val(countryList[selectedIndex]);
+				}
+			});				
+		});
+	});
+});
+
 $("#add-entry").on("click", function(event) {
     event.preventDefault();
-    var seached = $("#city-input").val().trim();
-      
-	//$(#ofinputfeild).html(val().trim)
-	var queryURL = "http://api.wunderground.com/api/eb190ccc88c7b1f8/geolookup/conditions/q/IA/" + seached + ".json";
+    resetResults();
+    var citySearch = $("#city-input").val().trim();
+    var countrySearch = $("#country-input").val().trim();
+ 	var queryURL = "http://api.wunderground.com/api/eb190ccc88c7b1f8/geolookup/conditions/q/" + countrySearch + "/" + citySearch + ".json";
 	
 	$.ajax({
 	  	url : queryURL,
-	  	method: "GET"
-	
+	  	method: "GET"	
 	}).done(function(response) {
 		var city = response.location.city;
 		var country = response.location.country;
@@ -24,7 +73,7 @@ $("#add-entry").on("click", function(event) {
 		console.log(country);
 		console.log(temp_f);
 		console.log(temp_c);
+		resetEntries();
 	});
 });
-	
-	
+
