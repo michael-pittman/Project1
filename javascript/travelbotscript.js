0728c54709dbf12f58d31a2aa12b2d7181096941
michaelc.pittman <michaelc.pittman@gmail.com>
2017-07-06 21:07:04 -0400

    adding field ID's for chat code

--- javascript/travelbotscript.js
+++ javascript/travelbotscript.js
@@ -169,3 +169,71 @@ $("#add-entry").on("click", function(event) {
 
 
   }
+
+
+
+//FireBase --------->
+// Initialize Firebase
+var config = {
+	apiKey: "AIzaSyCP7gyAk2MT2rY5JFD2i9b0WDHXbGUn7mQ",
+  authDomain: "project1-1499294091466.firebaseapp.com",
+  databaseURL: "https://project1-1499294091466.firebaseio.com",
+  projectId: "project1-1499294091466",
+  storageBucket: "",
+  messagingSenderId: "258284106684"
+};
+
+firebase.initializeApp(config);
+
+var dataRef = firebase.database();
+
+// Initial Values
+var name = "";
+var email = "";
+var comment = "";
+
+// Capture Button Click
+$("#add-user").on("click", function(event) {
+	event.preventDefault();
+
+	// YOUR TASK!!!
+	// Code in the logic for storing and retrieving the most recent user.
+	// Don't forget to provide initial data to your Firebase database.
+	name = $("#name-input").val().trim();
+	email = $("#email-input").val().trim();
+	comment = $("#comment-input").val().trim();
+
+	// Code for the push
+	dataRef.ref().push({
+
+		name: name,
+		email: email,
+		comment: comment,
+		dateAdded: firebase.database.ServerValue.TIMESTAMP
+	});
+});
+
+// Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+dataRef.ref().on("child_added", function(childSnapshot) {
+
+	// Log everything that's coming out of snapshot
+
+	console.log(childSnapshot.val().name);
+	console.log(childSnapshot.val().email);
+	console.log(childSnapshot.val().comment);
+	console.log(childSnapshot.val().joinDate);
+
+	
+// Handle the errors
+}, function(errorObject) {
+	console.log("Errors handled: " + errorObject.code);
+});
+
+dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+
+	// Change the HTML to reflect
+	$("#name-display").html(snapshot.val().name);
+	$("#email-display").html(snapshot.val().email);
+	$("#age-display").html(snapshot.val().age);
+	$("#comment-display").html(snapshot.val().comment);
+});
